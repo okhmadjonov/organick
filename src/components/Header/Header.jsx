d77f9d20112d4b7d0037c5c39668a074ae3c92d5
unknown <oakhmadjonov1@gmail.com>
2023-08-07 11:38:58 +0500

    Header is done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,72 @@
 import React from "react";
-import { HeaderContainer, HeaderMenu } from "./Header.styled";
+import logo from "../../assets/img/Logo.png";
+import { GoChevronDown } from "react-icons/go";
+import { BiSearch } from "react-icons/bi";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import {
+  Cart,
+  CartIcon,
+  CartText,
+  HeaderContainer,
+  HeaderMenu,
+  Link,
+  List,
+  ListItem,
+  Logo,
+  LogoMenu,
+  Menu,
+  Search,
+  SearchCart,
+  SearchField,
+  SearchIcon,
+} from "./Header.styled";
 const Header = () => {
   return (
     <HeaderContainer>
       <HeaderMenu>
-        <h2>Hello Header</h2>
+        <LogoMenu>
+          <Logo>
+            <img src={logo} alt="Logo" />
+          </Logo>
+          <Menu>
+            <List>
+              <ListItem>
+                <Link to="/">Home</Link>
+              </ListItem>
+              <ListItem>
+                <Link to="/about">About</Link>
+              </ListItem>
+              <ListItem>
+                <Link to="/pages">
+                  Pages <GoChevronDown style={{ marginTop: "4px" }} />
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link to="/shop">Shop</Link>
+              </ListItem>
+              <ListItem>
+                <Link to="/projects">Projects</Link>
+              </ListItem>
+              <ListItem>
+                <Link to="/News">News</Link>
+              </ListItem>
+            </List>
+          </Menu>
+        </LogoMenu>
+        <SearchCart>
+          <Search>
+            <SearchField></SearchField>
+            <SearchIcon>
+              <BiSearch />
+            </SearchIcon>
+          </Search>
+          <Cart>
+            <CartIcon>
+              <AiOutlineShoppingCart />
+            </CartIcon>
+            <CartText>Cart (0)</CartText>
+          </Cart>
+        </SearchCart>
       </HeaderMenu>
     </HeaderContainer>
   );
--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { NavLink } from "react-router-dom";
 
 export const HeaderContainer = styled.div`
   width: 100%;
@@ -13,5 +14,127 @@ export const HeaderMenu = styled.div`
   width: 83%;
   height: 66px;
   margin: 0 auto;
-  border: 1px solid black;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  /* border: 1px solid black; */
+`;
+export const LogoMenu = styled.div`
+  width: 57%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  /* border: 1px solid red; */
+`;
+
+export const Logo = styled.div`
+  width: 22%;
+  height: 100%;
+  /* border: 1px solid black; */
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+`;
+
+export const Menu = styled.div`
+  width: 58%;
+  height: 100%;
+  /* border: 1px solid black; */
+`;
+export const List = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  list-style-type: none;
+  cursor: pointer;
+`;
+
+export const ListItem = styled.li`
+  height: 66px;
+`;
+
+export const Link = styled(NavLink)`
+  height: 100%;
+  color: #274c5b;
+  font-size: 1.25em;
+  font-weight: 700;
+  display: flex;
+  align-items: center;
+  &:hover {
+    transition: all 0.3s ease-in-out;
+    color: #7eb693;
+  }
+`;
+
+export const SearchCart = styled.div`
+  width: 34%;
+  height: 100%;
+  display: flex;
+  justify-content: space-around;
+  /* border: 1px solid black; */
+`;
+
+export const Search = styled.div`
+  width: 67%;
+  height: 100%;
+  border-radius: 36px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background-color: #fafafa;
+  cursor: pointer;
+  /* border: 1px solid black; */
+`;
+
+export const SearchField = styled.input`
+  width: 75%;
+  height: 70%;
+  background: none;
+  font-size: 1em;
+  outline: none !important;
+  border: none;
+  padding-left: 20px;
+`;
+export const SearchIcon = styled.div`
+  width: 56px;
+  height: 56px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-size: 2em;
+  background-color: #7eb693;
+`;
+
+export const Cart = styled.div`
+  width: 30%;
+  height: 100%;
+  border-radius: 36px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  border: 1px solid #e0e0e0;
+  cursor: pointer;
+`;
+
+export const CartIcon = styled.div`
+  width: 56px;
+  height: 56px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-size: 2em;
+  background-color: #274c5b;
+`;
+
+export const CartText = styled.p`
+  font-size: 1.125em;
+  font-family: "Robot", sans-serif;
+  font-weight: 600;
+  line-height: 21.09px;
+  color: #274c5b;
 `;
